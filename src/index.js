7b8d810c4474df070b73a73489d9c902ffa389e5
Najmul-Islam <najmulislam519@gmail.com>
2022-03-11 01:09:10 +0600

    delete redux stup

--- src/index.js
+++ src/index.js
@@ -4,22 +4,17 @@ import { MovieProvider } from "./context/MoviesContext";
 import { TvProvider } from "./context/TvContext";
 import { BrowserRouter } from "react-router-dom";
 
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <MovieProvider>
-        <TvProvider>
-          <App />
-        </TvProvider>
-      </MovieProvider>
-    </Provider>
+    <MovieProvider>
+      <TvProvider>
+        <App />
+      </TvProvider>
+    </MovieProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
